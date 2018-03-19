9ee907516f08a60da6fe027ae75282696099aac0
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-19 15:19:24 -0500

    fixed a few errors mainly moving the keyfile to the install dir, will move later

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,18 @@ use std::process;
 fn main() {
     //read key from file
 
-    let temp = File::open("~/SlackToken");
+    let temp = File::open("SlackToken");
     let mut file;
     if temp.is_ok() {
         file = temp.ok().unwrap();
     } else {
-        print!("Cant open token file, {}", temp.err().unwrap());
+        println!("Cant open token file, {}", temp.err().unwrap());
         process::exit(1);
     }
     let mut token = String::new();
     let _ = file.read_to_string(&mut token);
-    
+    println!("your Slack token is \"{}\"", token);
+
     //rtm.connect
     //read res from ^
     //open webhook url from res
