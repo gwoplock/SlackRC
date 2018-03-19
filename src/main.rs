d18f43cda092aa76cf346ef5666eb605308a0a4b
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-19 15:13:52 -0500

    open token file and read to string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+
 fn main() {
     //read key from file
+
+    let temp = File::open("~/SlackToken");
+    let mut file;
+    if temp.is_ok() {
+        file = temp.ok().unwrap();
+    } else {
+        print!("Cant open token file, {}", temp.err().unwrap());
+        process::exit(1);
+    }
+    let mut token = String::new();
+    let _ = file.read_to_string(&mut token);
+    
     //rtm.connect
     //read res from ^
     //open webhook url from res
