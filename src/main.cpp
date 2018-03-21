9806bf8847deb860246abd74a2ee2183712d8fb0
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-21 16:29:41 -0500

    read key from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 int main(){
     //read key from file
+    std::ifstream keyFile;
+    //todo take file from cmd line
+    keyFile.open("../SlackToken");
+    if (!keyFile.is_open()){
+        std::cerr << "error opening file" << std::endl;
+        exit(1);
+    }
+    std::string key;
+    keyFile >> key;
+    std:: cout << key;
+    keyFile.close();
     //rtm.connect
+    std::string url;
+    url = "https://www.slack.com/api/rtm.connect?token=" + key;
     //read res from ^
     //open webhook url from res
     //print messages as the come
