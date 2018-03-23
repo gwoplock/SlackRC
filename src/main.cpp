0352416c4bc19a1bcf0068978ac28f570d19721c
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-23 16:27:34 -0500

    moving to linux box to try building

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,35 @@
 #include <boost/network/uri.hpp>
 #include <boost/network/uri/uri_io.hpp>
 #include <boost/network/uri/uri.ipp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 
 //NOTE: copied from "https://curl.haxx.se/libcurl/c/getinmemory.html"
 
 struct MemoryStruct
 {
-    char *memory;
-    size_t size;
+	char *memory;
+	size_t size;
 };
 
 static size_t
 WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    size_t realsize = size * nmemb;
-    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+	size_t realsize = size * nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *) userp;
 
-    mem->memory = (char *)realloc(mem->memory, mem->size + realsize + 1);
-    if (mem->memory == NULL)
-    {
-        /* out of memory! */
-        printf("not enough memory (realloc returned NULL)\n");
-        return 0;
-    }
+	mem->memory = (char *) realloc(mem->memory, mem->size + realsize + 1);
+	if (mem->memory == NULL) {
+		/* out of memory! */
+		printf("not enough memory (realloc returned NULL)\n");
+		return 0;
+	}
 
-    memcpy(&(mem->memory[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->memory[mem->size] = 0;
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
 
-    return realsize;
+	return realsize;
 }
 
 //END OF COPIED CODE
@@ -48,115 +49,127 @@ using tcp = boost::asio::ip::tcp;              // from <boost/asio/ip/tcp.hpp>
 namespace websocket = boost::beast::websocket; // from <boost/beast/websocket.hpp>
 
 //END OF COPIED CODE
-
+//NOTE: copied from "http://www.boost.org/doc/libs/develop/libs/beast/example/websocket/client/sync-ssl/websocket_client_sync_ssl.cpp"
+namespace ssl = boost::asio::ssl;               // from <boost/asio/ssl.hpp>
+//END copied
 int main()
 {
-    //read key from file
+	try {//read key from file
 
-    std::ifstream keyFile;
-    //todo take file from cmd line
-    keyFile.open("SlackToken");
-    if (!keyFile.is_open())
-    {
-        std::cerr << "error opening file" << std::endl;
-        exit(1);
-    }
-    std::string key;
-    keyFile >> key;
-    std::cout << "they key read is: " << key << std::endl;
-    keyFile.close();
+		std::ifstream keyFile;
+		//todo take file from cmd line
+		keyFile.open("../SlackToken");
+		if (!keyFile.is_open()) {
+			std::cerr << "error opening file" << std::endl;
+			exit(1);
+		}
+		std::string key;
+		keyFile >> key;
+		std::cout << "they key read is: " << key << std::endl;
+		keyFile.close();
 
-    //rtm.connect
+		//rtm.connect
 
-    std::string url;
-    url = "https://www.slack.com/api/rtm.connect?token=" + key;
+		std::string url;
+		url = "https://www.slack.com/api/rtm.connect?token=" + key;
 
-    CURL *curl = curl_easy_init();
-    if (!curl)
-    {
-        std::cerr << "error initing curl" << std::endl;
-        exit(1);
-    }
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		CURL *curl = curl_easy_init();
+		if (!curl) {
+			std::cerr << "error initing curl" << std::endl;
+			exit(1);
+		}
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
-    //NOTE: copied from "https://curl.haxx.se/libcurl/c/getinmemory.html"
+		//NOTE: copied from "https://curl.haxx.se/libcurl/c/getinmemory.html"
 
-    MemoryStruct chunk;
+		MemoryStruct chunk;
 
-    chunk.memory = (char *)malloc(1); /* will be grown as needed by the realloc above */
-    chunk.size = 0;                   /* no data at this point */
+		chunk.memory = (char *) malloc(1); /* will be grown as needed by the realloc above */
+		chunk.size = 0;                   /* no data at this point */
 
-    /* send all data to this function  */
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		/* send all data to this function  */
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
-    /* we pass our 'chunk' struct to the callback function */
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+		/* we pass our 'chunk' struct to the callback function */
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
 
-    /* some servers don't like requests that are made without a user-agent
-     field, so we provide one */
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-    //END OF COPIED CODE
+		/* some servers don't like requests that are made without a user-agent
+		 field, so we provide one */
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+		//END OF COPIED CODE
 
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
-    {
-        std::cerr << "error running curl " << curl_easy_strerror(res) << std::endl;
-        exit(1);
-    }
-    std::cout << "size receved: " << chunk.size << std::endl;
-    std::cout << "got: " << chunk.memory << std::endl;
+		CURLcode res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			std::cerr << "error running curl " << curl_easy_strerror(res) << std::endl;
+			exit(1);
+		}
+		std::cout << "size receved: " << chunk.size << std::endl;
+		std::cout << "got: " << chunk.memory << std::endl;
 
-    //read res from ^
+		//read res from ^
 
-    std::stringstream jsonSS;
-    jsonSS << chunk.memory;
+		std::stringstream jsonSS;
+		jsonSS << chunk.memory;
 
-    boost::property_tree::ptree pt;
-    boost::property_tree::read_json(jsonSS, pt);
-    std::string webhookURL = pt.get<std::string>("url");
-    std::cout << "url is: " << webhookURL << std::endl;
+		boost::property_tree::ptree pt;
+		boost::property_tree::read_json(jsonSS, pt);
+		std::string webhookURL = pt.get<std::string>("url");
+		std::cout << "url is: " << webhookURL << std::endl;
 
-    //parse webook url into host and path
+		//parse webook url into host and path
 
-    boost::network::uri::uri webhookURI(webhookURL);
+		boost::network::uri::uri webhookURI(webhookURL);
 
-    //open webhook url from res
+		//open webhook url from res
 
-    //NOTE: coppied from "http://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/quick_start.html"
-    // The io_context is required for all I/O
-    boost::asio::io_context ioc;
+		//NOTE: coppied from "http://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/quick_start.html"
+		// The io_context is required for all I/O
+		boost::asio::io_context ioc;
 
-    // These objects perform our I/O
-    tcp::resolver resolver{ioc};
-    websocket::stream<tcp::socket> ws{ioc};
-    std::cout << "host is: " << webhookURI.host() << std::endl;
-    auto const results = resolver.resolve(webhookURI.host(), "443");
+		// These objects perform our I/O
+		tcp::resolver resolver{ioc};
+		websocket::stream <tcp::socket> ws{ioc};
+		std::cout << "host is: " << webhookURI.host() << std::endl;
+		auto const results = resolver.resolve(webhookURI.host(), "443");
 
-    std::string fullPath = webhookURI.path() + "?" + webhookURI.query();
-    std::cout << "full path is: " << fullPath << std::endl;
+		std::string fullPath = webhookURI.path() /*+ "?" + webhookURI.query()*/;
+		std::cout << "full path is: " << fullPath << std::endl;
+		boost::beast::multi_buffer buffer;
+		// Make the connection on the IP address we get from a lookup
+		boost::asio::connect(ws.next_layer(), results.begin(), results.end());
 
-    // Make the connection on the IP address we get from a lookup
-    boost::asio::connect(ws.next_layer(), results.begin(), results.end());
+		//NOTE: taken from http://www.boost.org/doc/libs/develop/libs/beast/example/websocket/client/sync-ssl/websocket_client_sync_ssl.cpp
+		// Perform the SSL handshake
+		ws.next_layer().handshake(ssl::stream_base::client);
+		//END COPIED CODE
 
-    // Perform the websocket handshake
+		// Perform the websocket handshake
+		ws.handshake(webhookURI.host(), fullPath);
+		std::cout << "handshake done" << std::endl;
+		ws.write(boost::asio::buffer(std::string("{\n"
+				                                         "    \"id\": 1,\n"
+				                                         "    \"type\": \"ping\",\n"
+				                                         "    \"time\": 1403299273342\n"
+				                                         "}")));
 
-    ws.handshake(webhookURI.host(), fullPath);
+		// Read a message into our buffer
+		while (true) {
+			ws.read(buffer);
+			std::cout << boost::beast::buffers(buffer.data()) << std::endl;
+		}
+		// Close the WebSocket connection
+		ws.close(websocket::close_code::normal);
 
-    boost::beast::multi_buffer buffer;
+		// If we get here then the connection is closed gracefully
 
-    // Read a message into our buffer
-    while(true){
-        ws.read(buffer);
-        std::cout << boost::beast::buffers(buffer.data()) << std::endl;
-    }
-    // Close the WebSocket connection
-    ws.close(websocket::close_code::normal);
+		// The buffers() function helps print a ConstBufferSequence
+		std::cout << boost::beast::buffers(buffer.data()) << std::endl;
 
-    // If we get here then the connection is closed gracefully
-
-    // The buffers() function helps print a ConstBufferSequence
-    std::cout << boost::beast::buffers(buffer.data()) << std::endl;
-
-    //END COPIED CODE
-    //print messages as the come
+		//END COPIED CODE
+	} catch (std::exception const& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	//print messages as the come
 }
