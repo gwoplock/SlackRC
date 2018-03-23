d87a776c1697694b5efb6d467512cd90fc6f63f0
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-23 12:58:04 -0500

    open the socket to the URL returned from connect

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <fstream>
 #include <string>
 #include <curl/curl.h>
-#include <boost/beast.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/network/uri.hpp>
+#include <boost/network/uri/uri_io.hpp>
+#include <boost/network/uri/uri.ipp>
 
 //NOTE: copied from "https://curl.haxx.se/libcurl/c/getinmemory.html"
+
 struct MemoryStruct
 {
     char *memory;
@@ -35,9 +42,17 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 
 //END OF COPIED CODE
 
+//NOTE: coppied from "http://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/quick_start.html"
+
+using tcp = boost::asio::ip::tcp;              // from <boost/asio/ip/tcp.hpp>
+namespace websocket = boost::beast::websocket; // from <boost/beast/websocket.hpp>
+
+//END OF COPIED CODE
+
 int main()
 {
     //read key from file
+
     std::ifstream keyFile;
     //todo take file from cmd line
     keyFile.open("SlackToken");
@@ -50,7 +65,9 @@ int main()
     keyFile >> key;
     std::cout << "they key read is: " << key << std::endl;
     keyFile.close();
+
     //rtm.connect
+
     std::string url;
     url = "https://www.slack.com/api/rtm.connect?token=" + key;
 
@@ -65,9 +82,9 @@ int main()
     //NOTE: copied from "https://curl.haxx.se/libcurl/c/getinmemory.html"
 
     MemoryStruct chunk;
- 
-    chunk.memory = (char*) malloc(1);  /* will be grown as needed by the realloc above */ 
-    chunk.size = 0;    /* no data at this point */ 
+
+    chunk.memory = (char *)malloc(1); /* will be grown as needed by the realloc above */
+    chunk.size = 0;                   /* no data at this point */
 
     /* send all data to this function  */
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
@@ -81,20 +98,63 @@ int main()
     //END OF COPIED CODE
 
     CURLcode res = curl_easy_perform(curl);
-    if(res != CURLE_OK){
+    if (res != CURLE_OK)
+    {
         std::cerr << "error running curl " << curl_easy_strerror(res) << std::endl;
         exit(1);
     }
     std::cout << "size receved: " << chunk.size << std::endl;
-    std::cout << "got: " << chunk.memory <<std::endl;
+    std::cout << "got: " << chunk.memory << std::endl;
+
     //read res from ^
+
     std::stringstream jsonSS;
     jsonSS << chunk.memory;
 
     boost::property_tree::ptree pt;
-  boost::property_tree::read_json(jsonSS, pt);
-  std::cout << "url is" << pt.get<std::string>("url") << std::endl;
+    boost::property_tree::read_json(jsonSS, pt);
+    std::string webhookURL = pt.get<std::string>("url");
+    std::cout << "url is: " << webhookURL << std::endl;
+
+    //parse webook url into host and path
+
+    boost::network::uri::uri webhookURI(webhookURL);
 
     //open webhook url from res
+
+    //NOTE: coppied from "http://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/quick_start.html"
+    // The io_context is required for all I/O
+    boost::asio::io_context ioc;
+
+    // These objects perform our I/O
+    tcp::resolver resolver{ioc};
+    websocket::stream<tcp::socket> ws{ioc};
+    std::cout << "host is: " << webhookURI.host() << std::endl;
+    auto const results = resolver.resolve(webhookURI.host(), "80");
+
+    std::string fullPath = webhookURI.path() + "?" + webhookURI.query() + webhookURI.fragment();
+    std::cout << "full path is: " << fullPath << std::endl;
+
+    // Make the connection on the IP address we get from a lookup
+    boost::asio::connect(ws.next_layer(), results.begin(), results.end());
+
+    // Perform the websocket handshake
+
+    ws.handshake(webhookURI.host(), fullPath);
+
+    boost::beast::multi_buffer buffer;
+
+    // Read a message into our buffer
+    ws.read(buffer);
+
+    // Close the WebSocket connection
+    ws.close(websocket::close_code::normal);
+
+    // If we get here then the connection is closed gracefully
+
+    // The buffers() function helps print a ConstBufferSequence
+    std::cout << boost::beast::buffers(buffer.data()) << std::endl;
+
+    //END COPIED CODE
     //print messages as the come
 }
